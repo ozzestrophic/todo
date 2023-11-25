c7e49a42c966269094e7bb0bd4894d46f8e21309
Ozzeek <osama.m.alnagar@gmail.com>
2023-11-25 23:16:32 +0200

    added new project button functionality

--- src/sideBar.js
+++ src/sideBar.js
@@ -15,6 +15,61 @@ function createSideBarList() {
 
     projectUList.appendChild(listItem);
   }
+
+  const addProjectButton = document.createElement("button");
+  addProjectButton.innerText = "+ add project";
+  addProjectButton.classList.add("btn");
+  addProjectButton.classList.add("cancel");
+  addProjectButton.addEventListener("click", showNewProjectDialog);
+
+  projectUList.appendChild(addProjectButton);
+}
+
+function showNewProjectDialog() {
+  const newProjectDialog = createNewProjectDialog();
+  const projectUList = document.querySelector("#projectUList");
+
+  projectUList.appendChild(newProjectDialog);
+  newProjectDialog.showModal();
+}
+
+function addNewProjectToArray(event) {
+  event.preventDefault();
+  const textBox = document.querySelector("#newProjectTextBox");
+  confirmBtn.myParam = textBox.value;
+  arrayOfProjects.push(textBox.value);
+  createSideBarList();
+}
+
+function createNewProjectDialog() {
+  const newProjectDialog = document.createElement("dialog");
+  newProjectDialog.id = "newProjectDialogBox";
+  const newProjectForm = document.createElement("form");
+  newProjectDialog.appendChild(newProjectForm);
+
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("flex");
+  const newLabel = document.createElement("label");
+  newLabel.for = "newProject";
+  newLabel.innerText = "New Project Name";
+  const textBox = document.createElement("input");
+  textBox.required = true;
+  textBox.type = "text";
+  textBox.name = "newProject";
+  textBox.id = "newProjectTextBox";
+  textBox.classList.add("textBox");
+
+  newDiv.appendChild(newLabel);
+  newDiv.appendChild(textBox);
+  newProjectForm.appendChild(newDiv);
+
+  const confirmBtn = document.createElement("button");
+  confirmBtn.classList.add("newPrjBtn");
+  confirmBtn.innerText = "Create Project";
+  confirmBtn.addEventListener("click", addNewProjectToArray);
+  newProjectForm.appendChild(confirmBtn);
+
+  return newProjectDialog;
 }
 
 export { createSideBarList };
